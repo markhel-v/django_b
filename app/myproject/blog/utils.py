8956147857334a14731a308491f6a6a4c1044d68
Marshal <trash.66@inbox.ru>
2022-04-04 23:08:58 +0300

    add crud(admin_panel)

--- app/myproject/blog/utils.py
+++ app/myproject/blog/utils.py
@@ -53,13 +53,14 @@ class ObjectUpdateMixin():
 
 
 class ObjectDetailMixin:
+    
     model = None
     template = None
     
     def get(self,request, slug):
 
         obj = get_object_or_404(self.model,slug__iexact=slug)
-        return render(request,self.template,context = {self.model.__name__.lower(): obj})
+        return render(request,self.template,context = {self.model.__name__.lower(): obj , 'admin_object':obj , 'detail': True})
         
         
 class ObjectDeleteMixin:
